f2aeb08e4b9ff158609fabebeb879e1bcd3964ef
YuShiang <asdc861213@gmail.com>
2020-02-03 02:35:46 +0800

    myapp

--- django2_line_bot/LineBot/views.py
+++ django2_line_bot/LineBot/views.py
@@ -11,8 +11,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, FollowEve
 
 logger = logging.getLogger("django")
 
-line_bot_api = LineBotApi('Ps1DeC+Mswm2Zpd14q3sa/3/I1g5+MiiqiHD8mqPNa8ykCuQ7wJ+tIuJE6qVdf6uNNGge1Fzk+nwmZAc8s/LyMcEQK7wwSKEBIHQCLEo00YyRiwgE1FT8I28s2pc2Qs+mNVMNzBC316oal2zyMhfdgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('3882dd8561c35fbbe0d9b4f53912da7b')
+line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(settings.CHANNEL_SECRET)
 
 message="歡迎來到中山大學之道，本官方帳號會自動在當日提醒您今天有大學之道的演講或者是英文自學園的講座，避免您錯過而影響畢業門檻，如要查詢大學之道或英文自學園的講座日期，請在訊息欄輸入「大學之道」或「英文自學園」我們將為您提供資訊。"
 
