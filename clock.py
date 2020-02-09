770884b566da867066b38642ceb231422413ee8c
YuShiang <asdc861213@gmail.com>
2020-02-10 01:01:42 +0800

    line

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from linebot.models import TextSendMessage
 import requests
 import time
 
-CHANNEL_ACCESS_TOKEN = "---------------------------"
+CHANNEL_ACCESS_TOKEN = "Ps1DeC+Mswm2Zpd14q3sa/3/I1g5+MiiqiHD8mqPNa8ykCuQ7wJ+tIuJE6qVdf6uNNGge1Fzk+nwmZAc8s/LyMcEQK7wwSKEBIHQCLEo00YyRiwgE1FT8I28s2pc2Qs+mNVMNzBC316oal2zyMhfdgdB04t89/1O/w1cDnyilFU="
 line_bot_api=LineBotApi(CHANNEL_ACCESS_TOKEN)
 def auto_broadcast():
     line_bot_api.broadcast(TextSendMessage(text='同學早安阿!'))
