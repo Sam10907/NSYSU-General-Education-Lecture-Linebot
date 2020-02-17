772a339715b94c4b5fc397039f7df12fbec2b6b7
Sam10907 <42615337+Sam10907@users.noreply.github.com>
2020-02-18 01:22:40 +0800

    Update models.py

--- django2_line_bot/LineBot/models.py
+++ django2_line_bot/LineBot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User_data(models.Model):
+    user_id=models.CharField(max_length=200)
+    text_index=models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{} : {}'.format(self.user_id,self.text_index)
