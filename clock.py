97b01a385ccf31fd53a804b35ba5536431444922
Sam10907 <42615337+Sam10907@users.noreply.github.com>
2020-02-18 01:19:33 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,18 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-import requests
 import time
 
 CHANNEL_ACCESS_TOKEN = "--------------"
 line_bot_api=LineBotApi(CHANNEL_ACCESS_TOKEN)
 def auto_broadcast():
     line_bot_api.broadcast(TextSendMessage(text='同學早安阿!'))
-def request(): #request heroku per 25 minutes so as not to sleep
-    url='https://mychatbotproject.herokuapp.com/'
-    requests.get(url)
 
 sched=BlockingScheduler()
 sched.add_job(auto_broadcast,'interval',days=1, start_date='2020-2-5 01:00:00')
-sched.add_job(request,'interval',minutes=25)
 sched.start()
